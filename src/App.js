9d9a6ab9abb0ac43e0d89d59cd5b68e3e2f00202
Your Name <you@example.com>
2024-06-20 14:27:49 -0400

    usestate function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import './App.css';
 
 import ParkingLotForm from './components/ParkingLotForm/ParkingLotForm';
 import ParkingLotList from './components/ParkingLotList/ParkingLotList';
 
 function App() {
+
+  let [parkingLotItems, setParkingLotItems] = useState([
+    {
+      date: '2024-06-01',
+      priority: 'Low',
+      link: 'https://www.google.com',
+      description: 'Ultimate source of truth',
+    },
+    {
+      date: '2024-05-31',
+      priority: 'Medium',
+      link: 'https://react.dev',
+      description: 'React documentation and tutorial',
+    }
+  ]);
+
   return (
     <div className="App">
       <header>
         <h1>Browser Parking Lot</h1>
+        <p>Kiss those tabs goodbye.</p>
       </header>
       <main>
         <ParkingLotForm />
