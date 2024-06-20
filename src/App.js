0ae90469e0780f83b5e2edd5992f6067c864f2c8
Your Name <you@example.com>
2024-06-20 13:16:51 -0400

    parking lot form and list empty components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
+import ParkingLotForm from './components/ParkingLotForm/ParkingLotForm';
+import ParkingLotList from './components/ParkingLotList/ParkingLotList';
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Browser Parking Lot</h1>
       </header>
+      <main>
+        <ParkingLotForm />
+        <ParkingLotList />
+      </main>
     </div>
   );
 }
