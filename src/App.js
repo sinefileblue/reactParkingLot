1c926ea39b6fc9f51e96f73b93977bc4b6b19031
Your Name <you@example.com>
2024-06-26 15:24:14 -0400

    update parkinglotforms/lists and styling

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
-import './App.css';
+import "./App.css";
 
-import ParkingLotForm from './components/ParkingLotForm/ParkingLotForm';
-import ParkingLotList from './components/ParkingLotList/ParkingLotList';
+import ParkingLotForm from "./components/ParkingLotForm/ParkingLotForm";
+import ParkingLotList from "./components/ParkingLotList/ParkingLotList";
 
-function App() {
+export default function App() {
+    let [parkingLotItems, setParkingLotItems] = useState([
+      {
+        id: nanoid(),
+        date: "2024-06-01",
+        priority: "Low",
+        link: "https://www.google.com",
+        description: "Ultimate source of truth",
+      },
+      {
+        id: nanoid(),
+        date: "2024-05-31",
+        priority: "Medium",
+        link: "https://react.dev",
+        description: "React documentation and tutorial",
+      }
+    ]);
 
-  let [parkingLotItems, setParkingLotItems] = useState([
-    {
-      id: nanoid(),
-      date: '2024-06-01',
-      priority: 'Low',
-      link: 'https://www.google.com',
-      description: 'Ultimate source of truth',
-    },
-    {
-      id: nanoid(),
-      date: '2024-05-31',
-      priority: 'Medium',
-      link: 'https://react.dev',
-      description: 'React documentation and tutorial',
-    }
-  ]);
-
-  return (
-    <div className="App">
-      <header>
-        <h1>Browser Parking Lot</h1>
-        <p>Kiss those tabs goodbye.</p>
-      </header>
-      <main>
-        <ParkingLotForm />
-        <ParkingLotList parkingLotItems={parkingLotItems}/>
-      </main>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div className="App">
+        <header>
+          <h1>Browser Parking Lot</h1>
+          <p>Kiss those tabs goodbye.</p>
+        </header>
+        <main>
+          <ParkingLotForm />
+          <ParkingLotList parkingLotItems={parkingLotItems}/>
+        </main>
+      </div>
+    );
+  }
--- src/components/ParkingLotForm/ParkingLotForm.jsx
+++ src/components/ParkingLotForm/ParkingLotForm.jsx
@@ -1,7 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button, FormGroup, Label, Input } from 'reactstrap';
+import './ParkingLotForm.css';
+
+const PRIORITIES = {
+    Low: 'Low',
+    Medium: 'Medium',
+    High: 'High',
+}
+
+
 
 export default function ParkingLotForm() {
+
+    const [date, setDate] = useState('');
+    const [link, setLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [priority, setPriority] = useState(PRIORITIES.Medium);
+
+    function handleDateChange(e) {
+      setDate(e.target.value);
+    }
+
+    function handleLinkChange(e) {
+      setLink(e.target.value);
+    }
+
+    function handleDescriptionChange(e) {
+      setDescription(e.target.value);
+    }
+
   return (
-    <div>ParkingLotForm</div>
+    <Form data-bs-theme="dark" className="parking-lot-form">
+      <FormGroup className="parking-lot-row">
+        <Label hmtlFor= "link-date">Date</Label>
+        <Input
+            type="date"
+            name="date"
+            id="link-date"
+            value={date}
+            onChange={handleDateChange}
+            required
+        />
+      </FormGroup>
+      <FormGroup className="parking-lot-row">
+          <Label htmlFor="link-url">Link</Label>
+          <Input
+              type="url"
+              name="link"
+              id="link-url"
+              value={link}
+              onChange={handleLinkChange}
+              required
+          />
+      </FormGroup>
+      <FormGroup className="parking-lot-row">
+          <Label htmlFor="link-description">Description</Label>
+          <Input
+              type="text"
+              name="description"
+              id="link-description"
+              value={description}
+              onChange={handleDescriptionChange}
+              required
+          />
+      </FormGroup>
+      <FormGroup className="parking-lot-row">
+          <Input
+              type="radio"
+              name="radio-priority"
+              value={PRIORITIES.High}
+              checked={priority === PRIORITIES.High}
+              id="prio-high"
+          />
+          {'  '}
+          <Label htmlFor="prio-high"  className="me-3">
+            High
+          </Label>
+          <Input
+              type="radio"
+              name="radio-priority"
+              value={PRIORITIES.Medium}
+              checked={priority === PRIORITIES.Medium}
+              id="prio-medium"
+          />
+          {'  '}
+          <Label htmlFor="prio-medium"  className="me-3">
+            Medium
+          </Label>
+          <Input
+              type="radio"
+              name="radio-priority"
+              value={PRIORITIES.Low}
+              checked={priority === PRIORITIES.Low}
+              id="prio-low"
+          />
+          {'  '}
+          <Label htmlFor="prio-low" className="me-3">
+            Low
+          </Label>
+
+      </FormGroup>
+      <Button type="submit">
+          Submit        
+      </Button>
+    </Form>
   )
 }
