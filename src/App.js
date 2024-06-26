0525476f8e13188d73df96316294a9b722a201d3
Your Name <you@example.com>
2024-06-26 18:00:19 -0400

    app state clearing

--- src/App.js
+++ src/App.js
@@ -6,22 +6,7 @@ import ParkingLotForm from "./components/ParkingLotForm/ParkingLotForm";
 import ParkingLotList from "./components/ParkingLotList/ParkingLotList";
 
 export default function App() {
-    let [parkingLotItems, setParkingLotItems] = useState([
-      {
-        id: nanoid(),
-        date: "2024-06-01",
-        priority: "Low",
-        link: "https://www.google.com",
-        description: "Ultimate source of truth",
-      },
-      {
-        id: nanoid(),
-        date: "2024-05-31",
-        priority: "Medium",
-        link: "https://react.dev",
-        description: "React documentation and tutorial",
-      }
-    ]);
+    let [parkingLotItems, setParkingLotItems] = useState([]);
 
     function addItem(date, link, description, priority) {
       setParkingLotItems((oldItems) => [
